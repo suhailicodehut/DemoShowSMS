92579ff97721f6eb69a1a0d2ca5d38e6d79363e5
15017519 <15017519@myrp.edu.sg>
2017-06-02 11:59:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017519/demoshowsms/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a15017519.demoshowsms;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
+
